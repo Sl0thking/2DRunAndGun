95032e723a1d55f3f738ecd4aa97be710b683123
Joshua Ward <joshuaward.dev@gmail.com>
2018-05-26 23:56:57 +0200

    Prefabs and Projectile & Weapon changes
    + changed Projectile behavior
    + implemented a good Weapon base class
    + added Prefabs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,26 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof (Rigidbody2D))]
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
 
-	[Range (1, 100)]
-	public int damage = 10;
-	[Range (1, 10)]
-	public float travelSpeed = 1f;
+	private int damage = 10;
+	private float travelSpeed = 10f;
 
+	private GameObject instantiator;
 	private Rigidbody2D rigidBody;
 
-	void Start () {
+	private bool initialized = false;
+
+	void Start ()
+	{
 		rigidBody = this.GetComponent<Rigidbody2D>();
 	}
+
+	public void Initialize (GameObject instantiator, int damage, float travelSpeed)
+	{
+		this.instantiator = instantiator;
+		this.damage = damage;
+		this.travelSpeed = travelSpeed;
+		this.initialized = true;
+	}
 	
-	void Update () {
-		this.rigidBody.velocity = this.transform.right * -1 * travelSpeed;
+	void Update ()
+	{
+		if (initialized)
+		{
+			this.rigidBody.velocity = this.transform.right * -1 * travelSpeed;
+		}			
 	}
 
 	void OnTriggerEnter2D (Collider2D other)
     {
-		if (other.gameObject.GetComponent<Projectile>() == null)
+		if (this.instantiator != other.gameObject && other.gameObject.GetComponent<Projectile>() == null)
 		{
 			var health = other.gameObject.GetComponent<Health>();
 			if (health != null)
