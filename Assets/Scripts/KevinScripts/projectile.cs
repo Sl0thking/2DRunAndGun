8b3eff768969df872375d9a9bac668f996757b27
KevinWesseler <xkivx@web.de>
2018-05-26 20:36:35 +0200

    Enemy and BulletHandling

---
+++ Assets/Scripts/KevinScripts/projectile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour {
+
+    public Rigidbody2D rigidbody;
+    public Vector2 velocity;
+    float direction = 1;
+    float speed = 5;
+    public float damage = 10;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+        print("EB: " + this.GetComponentInParent<EnemyBasicBehavior>());
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //velocity = new Vector2(direction * speed, rigidbody.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+
+        if(collision.collider.tag != "Player"){
+            print("COLLISION WITH SOMETHING != PLAYER");
+            rigidbody.velocity = new Vector2(-velocity.x, velocity.y);
+        } else {
+            PlayerController2D playerContr = (PlayerController2D) collision.gameObject.GetComponent<PlayerController2D>();
+            try{
+                //playerContr.takeDamage(damage);
+                //if (playerContr.getHealth() >= 0){
+                    Destroy(collision.gameObject);
+                //}
+            } catch(UnityException){}
+            Explode();
+        }
+    }
+
+    void Explode(){
+        print("DESTROY");
+        Destroy(this.gameObject);
+    }
+}
